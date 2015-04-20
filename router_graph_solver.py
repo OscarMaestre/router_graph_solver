55622e460ae37f00186486b3d0897894ee858941
Oscar G.G <profesor.oscar.gomez@gmail.com>
2015-04-20 21:05:42 +0200

    Included languages: english and spanish

--- router_graph_solver.py
+++ router_graph_solver.py
@@ -6,6 +6,10 @@ import sys
 import ipaddress
 
 
+LOGGING_ENABLED = False
+ES="espanol"
+EN="english"
+LANGUAGE=ES
 #This class is used to store IP addresses used by a router
 #to connect with other routers
 class Router(object):
@@ -13,22 +17,61 @@ class Router(object):
         self.name=name
         self.connected_routers=[]
         self.ip_addresses=[]
+        self.routes=[]
     def connect_to_router(self, router_name, ip_address):
         if router_name in self.connected_routers:
             return
         self.connected_routers.append(router_name)
         self.ip_addresses.append(ip_address)
-        print (self.name + " connected to " + router_name)
-        print (self.ip_addresses)
+    def append_route(self, route):
+        total=len(self.routes)
+        #Check if route is a better route
+        #than some of the router stored
+        for pos in range(0, total):
+            current_route=self.routes[pos]
+            if route.can_replace(current_route) or route.is_better_than(current_route):
+                if LOGGING_ENABLED:
+                    print ( "{} replaced by {}".format(current_route, route))
+                self.routes[pos]=route
+                return
+        if self.can_be_added(route):
+            #The route can't replace any other route, so let's append the new route
+            if LOGGING_ENABLED:
+                print ("Added route "+str(route) + " for "+self.name)
+            self.routes.append(route)
+    
+    #A route that uses the same gateway than other but with
+    #a worse metric shouldn't be added
+    def can_be_added(self, route):
+        for r in self.routes:
+            same_gw         = ( r.via             == route.via      )
+            worse_metric    = ( route.metric       > r.metric       )
+            same_destination= ( route.destination ==r.destination   )
+            if same_gw and worse_metric and same_destination:
+                return False
+        return True
+    
     def get_description(self):
-        print("Describing "+self.name)
+        print("Router "+self.name)
         text=""
         num=len(self.connected_routers)
         for pos in range(0, num):
             r=self.connected_routers[pos]
             addr=self.ip_addresses[pos]
-            text=text+"to "+r+" via "+str(addr)+"\n"
+            if LANGUAGE==EN:
+                text=text+self.name+" connected to "+r+" using "+str(addr)+"\n"
+            if LANGUAGE==ES:
+                text=text+self.name+" conectado con "+r+" usando la IP "+str(addr)+"\n"                
         return text
+    def get_routing_table(self):
+        if LANGUAGE==EN:
+            print ("Routing table for "+self.name)
+        if LANGUAGE==ES:
+            print ("Tabla de enrutamiento para "+self.name)
+        txt=""
+        for route in self.routes:
+            txt+=str(route) + "\n"
+        return txt
     def __str__(self):
         return self.name
     def get_name(self):
@@ -56,6 +99,7 @@ class RouterList:
             if r.get_name()==router_name:
                 return r
         return None
+    
     def __iter__(self):
         return self
     def __next__(self):
@@ -67,8 +111,45 @@ class RouterList:
             self.pos=0
             raise StopIteration()
     
-        
-
+class Route:
+    def __init__(self, destination, via, metric):
+        self.destination=destination
+        self.via=via
+        self.metric=metric
+    #Other route can replace this route?
+    def can_replace(self, route):
+        same_network    =   ( self.destination ==  route.destination    )
+        better_metric   =   ( self.metric      <   route.metric         )
+        same_gateway    =   ( self.via         ==  route.via            )
+        if same_network and better_metric and same_gateway :
+            if LOGGING_ENABLED:
+                print ( "{} can replace {}".format (self, route) )
+            return True
+        return False
+    def is_better_than(self, route):
+        if self.destination==route.destination and self.metric<route.metric:
+                if LOGGING_ENABLED:
+                    print ("{} is better than {}".format(self, route))
+                return True
+        if LOGGING_ENABLED:
+            print ("{} isn't better than {}".format(self, route))
+        return False
+    def __eq__(self, route):
+        if route.destination!=self.destination:
+            return False
+        if route.via!=self.via:
+            return False
+        if route.metric!=self.metric:
+            return False
+        return True
+    def __str__(self):
+        txt=""
+        if LANGUAGE==EN:
+            txt+="Destination {} via {} metric {}".format(self.destination, self.via, self.metric)
+        if LANGUAGE==ES:
+            txt+="Red de destino:{} Siguiente salto: {} metrica:{}".format(self.destination, self.via, self.metric)
+        return txt
+    
 def print_help():
     print ("\nUse: "+sys.argv[0]+" <file>")
     print ("Syntax error. Sections in <file> must have the syntax of A or B\n")
@@ -155,13 +236,35 @@ def parse_section(section):
             print ("Erroneous section:"+str(section))
 
 
+
+def get_metric (node1, node2):
+    #metrics=networkx.get_edge_attributes(graph, 'weight')
+    return int(graph[node1][node2]['weight'])
+
+def calculate_metric(path):    
+    if (len(path)==2):
+        return get_metric (path[0], path[1])
+    else:
+        return get_metric (path[0], path[1]) + calculate_metric(path[1:])
+        
+    
+
 def create_routing_table(router, graph, networks):
-    print ("Routing table for "+str(router))
+    
+    
     for network in networks:
-        print (network)
         paths=networkx.all_simple_paths(graph, str(router), network)
-        for p in paths:
-            print (p)
+        #Paths always contains the router passed, let's remove it
+        
+        for path in paths:
+            if (len(path)!=2):
+                gateway     =   router.get_ip_to_find_router(path[1])
+                destination =   network
+                metric      =   calculate_metric(path)
+                route       =   Route ( destination, gateway, int(metric) )
+                
+                router.append_route(route)
+                
     
 #Main program
 config=configparser.ConfigParser()
@@ -188,3 +291,6 @@ for router in routers:
     #given a graph of interconnections
     #finding ways to the different networks
     create_routing_table(router, graph, networks)
+    
+for route in routers:
+    print (route.get_routing_table())
